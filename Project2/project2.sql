0f86c75a1e1c7c662afb5231d67e0ab5de6f0df1
Ally Smith <allys7@protonmail.com>
2022-01-25 16:00:33 -0700

    Initial commit, starting on P2

---
+++ Project2/project2.sql
@@ -0,0 +1,21 @@
+/*
+ *  project2.sql
+ * 
+ *  author: C. Painter-Wakefield (please replace with your name)
+ *
+ */
+
+/* don't forget to set up your search path - in psql, do: */
+set search_path to project2;
+
+/* This next query lists all of the pioneers and related info. */
+SELECT * FROM people;
+
+/* This query returns a count of all the pioneers; here we are using */
+/* an aggregate function (COUNT), which we will look at in a future lecture. */
+SELECT COUNT(*) FROM people;
+
+/* Please delete the queries above and replace with your queries for       */
+/* project2!                                                               */
+
+-- Note you can also do single-line comments this way
